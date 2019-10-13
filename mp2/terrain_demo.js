0d8366f93f9b80b2a1d3fcffef0fb51b20898869
Finn Sinclair <finnnorth@gmail.com>
2019-10-13 10:53:26 -0500

    Further commenting and cleanup

--- mp2/terrain_demo.js
+++ mp2/terrain_demo.js
@@ -21,6 +21,7 @@ document.getElementById("toggle_sphere").onclick = () => {
 // This is not really a function, it's just the
 // mechanism for organizing the terrain demo as a
 // separate pseudo-"module".
+// The sketchy VanillaJS equivalent of module.exports
 terrain_demo = function() {
 
 
@@ -51,14 +52,21 @@ terrain_demo = function() {
         var rotateX = 0;
         var rotateY = 1000;
         function render(now) {
+
+            // Compute per-frame mouse input
             rotateX += mouseVelX;
             mouseVelX += -mouseVelX * 0.05;
             rotateY += mouseVelY;
             rotateY = clamp(rotateY, 500, 5000);
             mouseVelY += -mouseVelY * 0.05;
+
+            // Draw scene, pass in buffers
             draw(rotateX, rotateY * 0.001, bufferResult.numIndices, vertexPositionBuffer, vertexIndexBuffer, vertexNormalBuffer, terrainShaderProgram, logoGL);
+            
+            // Set the uniform value for the shininess based on slider input.
             logoGL.uniform1f(terrainShaderProgram.shiny, document.getElementById("shininess").value);
 
+            // Get blinn/phong toggle status and set the shader accordingly
             if(document.querySelector('input[name="use_blinnphong"]:checked').value == "yes"){
                 console.log("Using blinn-phong");
                 logoGL.uniform1f(terrainShaderProgram.useBlinnPhong, 1.0);
@@ -66,6 +74,8 @@ terrain_demo = function() {
                 logoGL.uniform1f(terrainShaderProgram.useBlinnPhong, 0.0);
             }
 
+            // If the regen_model flag was set, we regenerate the buffers with the useSphere flag,
+            // and then refresh all the other buffers.
             if(regenModel){
                 regenModel = false;
                 bufferResult = setupBuffers(logoGL.STATIC_DRAW, useSphere, logoGL);
@@ -77,6 +87,7 @@ terrain_demo = function() {
                 vertexNormalBuffer = bufferResult.normals;
             }
 
+            // Set the time uniform variable so that the lights can animate/spin smoothly around the scene :)
             logoGL.uniform1f(terrainShaderProgram.time, now);
             animationID = requestAnimationFrame(render);
         }
@@ -87,31 +98,36 @@ terrain_demo = function() {
     /**
      * Calculates transformation matrices and applies them to the given
      * shader program.
-     * @param {number} x X rotation
-     * @param {number} y Y angle
+     * @param {number} x X rotation (used to spin terrain)
+     * @param {number} y Y factor (used to dolly-zoom the camera)
      * @param {WebGLProgram} shaderProgram Reference to the shader program it will update uniform matrices for
      * @param {WebGLRenderingContext} gl Reference to the WebGL context
+     * @returns void
      */
     var calculate_matrices = function(x, y, shaderProgram, gl){
-        var modelViewMatrix = glMatrix.mat4.create();
 
+        // Construct perspective projection matrix.
         var projectionMatrix = glMatrix.mat4.create();
         glMatrix.mat4.perspective(projectionMatrix, Math.PI/5, gl.canvas.width/ gl.canvas.height, 1,150);
 
+        // Calculate a good eyepos to look at the terrain from.
         var eyePos = glMatrix.vec3.fromValues(y*10,y**1.5 * 5,y*10);
         
+        // Construct modelview matrix using a lookat and a y-rotation, plus a translation to
+        // give us some distance from the model.
+        var modelViewMatrix = glMatrix.mat4.create();
         glMatrix.mat4.lookAt(modelViewMatrix, eyePos, [0,1,0], [0,1,0]);
         glMatrix.mat4.rotateY(modelViewMatrix, modelViewMatrix, x/200);
         glMatrix.mat4.translate(modelViewMatrix, modelViewMatrix, [-(terrain_dim * 0.1 * 0.5), 0, -(terrain_dim * 0.1 * 0.5)]);
-        //
+        
 
-
+        // Construct normal matrix, which is simply the inverse transpose of the modelview.
         var normalMatrix = glMatrix.mat3.create();
         glMatrix.mat3.fromMat4(normalMatrix, modelViewMatrix);
         glMatrix.mat3.transpose(normalMatrix, normalMatrix);
         glMatrix.mat3.invert(normalMatrix, normalMatrix);
         
-        //glMatrix.mat4.scale(local, local, [0.5, 0.5, 0.5]);
+        // Load the matrices into the shader program!
         gl.uniformMatrix4fv(shaderProgram.modelview, false, modelViewMatrix);
         gl.uniformMatrix4fv(shaderProgram.projection, false, projectionMatrix);
         gl.uniformMatrix3fv(shaderProgram.normalMatrix, false, normalMatrix);
@@ -157,116 +173,113 @@ terrain_demo = function() {
     /**
      *  Sets up buffers with provided data
      *  Much of this is borrowed from the HelloColor/HelloTriangle example.
-     * @param {boolean} generateSphere Whether to override the terrain gen and just do a sphere!
      * @param {number} drawMode Drawing mode (STATIC_DRAW, DYNAMIC_DRAW)
+     * @param {boolean} generateSphere Whether to override the terrain gen and just do a sphere!
      * @param {WebGLRenderingContext} gl Reference to the WebGL context
      * @returns {object} Object with the position and index WebGLBuffers
      */
     var setupBuffers = function(drawMode, generateSphere, gl){
-
-        // Create an "adjacency map", similar to an adjacency matrix,
-        // but is a hashmap mapping each vertex to the other vertices
-        // that share a triangle with the hash key.
-        // This allows us to reduce the algorithmic complexity of the inverse
-        // lookup of shared polygons/edges, which is otherwise a quadratic complexity problem.
-        // There is a more clever way to do it than with an inverse
-        // mapping hashmap, but this should actually be relatively generalizable
-        // to other meshes that are not our restrictive planes/terrain meshes.
-        var adjacency_map = new Object();
-
-        // Construct the base plane vertex data, terrain_dim x terrain_dim
-        modelData = [];
-        for(var y = 0; y < terrain_dim; y++){
-            for(var x = 0; x < terrain_dim; x++){
-            
-                modelData.push(x*0.1, 0, y*0.1);
-            }
-        }
-        console.log(modelData.length/3 + " vertices");
-
-        // Construct the index/element array, while building up the nifty adjacency mapping
-        // while we link the vertices together. (That way, we only have to do that logic once.)
-        modelIndices = [];
-        
-        // Iterate through our already created terrain vertex array
-        for(var y = 0; y < terrain_dim-1; y++){
-            for(var x = 0; x < terrain_dim-1; x++){
-
-                // Calculate array index based on current x/y 2D indexing into the terrain
-                var index = x + y * terrain_dim;
-
-                // Push the actual indices. (Constructs two triangles, sharing two vertices
-                // each. This is important....)
-                modelIndices.push(index, index + terrain_dim, index + 1);
-                modelIndices.push(index + terrain_dim, index + 1 + terrain_dim, index + 1);
-
-                // Construct the "adjacency map" for BOTH of the current polygons, essentially
-                // providing a constant-time adjacency lookup for all vertices, to find
-                // their neighbors for normal calculations.
-                adjacency_map[index] = [modelIndices.length-3, modelIndices.length -6];
-                adjacency_map[index + terrain_dim] = [modelIndices.length-3, modelIndices.length -6];
-                adjacency_map[index + terrain_dim + 1] = [modelIndices.length-3, modelIndices.length -6];
-                adjacency_map[index + 1] = [modelIndices.length-3, modelIndices.length -6];
-            }   
-        }
-        console.log(modelIndices.length/3 + " triangles");
-
-        // Deform the terrain..
-        deform_terrain(modelData, terrain_dim, 1024);
-
-        // Squish the terrain altitude to [0,1], makes
-        // the whole color mapping thing a bit easier to wrangle.
-        normalize_mesh_height(modelData);
-
-
-        // Calculate face normals
-        var tri_norms = [];
-
-        // Iterate through all triangles
-        for(var tri_idx = 0; tri_idx < modelIndices.length; tri_idx += 3){
-
-            // Compute face/triangle normal
-            var tri_norm = compute_tri_normal(
-                modelData.slice(modelIndices[tri_idx] * 3, modelIndices[tri_idx] * 3 + 3),
-                modelData.slice(modelIndices[tri_idx+1] * 3, modelIndices[tri_idx+1] * 3 + 3),
-                modelData.slice(modelIndices[tri_idx+2] * 3, modelIndices[tri_idx+2] * 3 + 3))
-
-            tri_norms.push(tri_norm);
-        }
-
-        // Calculate vertex normals
+        // Various mesh data we will be generating.
         var vertexNormals = [];
+        var modelData = [];
+        var modelIndices = [];
 
-        // Iterate through every vertex
-        for(var y = 0; y < terrain_dim; y++){
-            for(var x = 0; x < terrain_dim; x++){
+        if(generateSphere == false){
 
-                // Calculate the array index
-                var vertex_index = x + y * terrain_dim;
+            // Create an "adjacency map", similar to an adjacency matrix,
+            // but is a hashmap mapping each vertex to the other vertices
+            // that share a triangle with the hash key.
+            // This allows us to reduce the algorithmic complexity of the inverse
+            // lookup of shared polygons/edges, which is otherwise a quadratic complexity problem.
+            // There is a more clever way to do it than with an inverse
+            // mapping hashmap, but this should actually be relatively generalizable
+            // to other meshes that are not our restrictive planes/terrain meshes.
+            // This represents an absurd exponential efficiency improvement.
+            var adjacency_map = new Object();
 
-                var normal_sum = glMatrix.vec3.create();
-
-                // Iterate through all adjacent (connected) vertices, aka,
-                // the vertices which share a triangle with this vertex.
-                // For each of the connected vert
-                adjacency_map[vertex_index].forEach((tri_idx) => {
-                    glMatrix.vec3.add(normal_sum, normal_sum, tri_norms[tri_idx/3]);
-                });
+            // Construct the base plane vertex data, terrain_dim x terrain_dim
+            for(var y = 0; y < terrain_dim; y++){
+                for(var x = 0; x < terrain_dim; x++){
                 
-                // Normalize vertex normal.
-                glMatrix.vec3.normalize(normal_sum, normal_sum);
-
-                vertexNormals.push(normal_sum[0], normal_sum[1], normal_sum[2]);
+                    modelData.push(x*0.1, 0, y*0.1);
+                }
             }
-        }
-        
-        
+            console.log(modelData.length/3 + " vertices");
 
-        if(generateSphere){
-            vertexNormals.length = 0;
-            modelData.length = 0;
-            modelIndices.length = 0;
+            // Construct the index/element array, while building up the nifty adjacency mapping
+            // while we link the vertices together. (That way, we only have to do that logic once.)
+            // We iterate through our already created terrain vertex array...
+            for(var y = 0; y < terrain_dim-1; y++){
+                for(var x = 0; x < terrain_dim-1; x++){
 
+                    // Calculate array index based on current x/y 2D indexing into the terrain
+                    var index = x + y * terrain_dim;
+
+                    // Push the actual indices. (Constructs two triangles, sharing two vertices
+                    // each. This is important....)
+                    modelIndices.push(index, index + terrain_dim, index + 1);
+                    modelIndices.push(index + terrain_dim, index + 1 + terrain_dim, index + 1);
+
+                    // Construct the "adjacency map" for BOTH of the current polygons, essentially
+                    // providing a constant-time adjacency lookup for all vertices, to find
+                    // their neighbors for normal calculations.
+                    adjacency_map[index] = [modelIndices.length-3, modelIndices.length -6];
+                    adjacency_map[index + terrain_dim] = [modelIndices.length-3, modelIndices.length -6];
+                    adjacency_map[index + terrain_dim + 1] = [modelIndices.length-3, modelIndices.length -6];
+                    adjacency_map[index + 1] = [modelIndices.length-3, modelIndices.length -6];
+                }   
+            }
+            console.log(modelIndices.length/3 + " triangles");
+
+            // Deform the terrain..
+            deform_terrain(modelData, terrain_dim, 1024);
+
+            // Squish the terrain altitude to [0,1], makes
+            // the whole color mapping thing a bit easier to wrangle.
+            normalize_mesh_height(modelData);
+
+
+            // Calculate face normals
+            var tri_norms = [];
+
+            // Iterate through all triangles
+            for(var tri_idx = 0; tri_idx < modelIndices.length; tri_idx += 3){
+
+                // Compute face/triangle normal
+                var tri_norm = compute_tri_normal(
+                    modelData.slice(modelIndices[tri_idx] * 3, modelIndices[tri_idx] * 3 + 3),
+                    modelData.slice(modelIndices[tri_idx+1] * 3, modelIndices[tri_idx+1] * 3 + 3),
+                    modelData.slice(modelIndices[tri_idx+2] * 3, modelIndices[tri_idx+2] * 3 + 3))
+
+                tri_norms.push(tri_norm);
+            }
+
+            // Calculate vertex normals
+            // Iterate through every vertex
+            for(var y = 0; y < terrain_dim; y++){
+                for(var x = 0; x < terrain_dim; x++){
+
+                    // Calculate the array index
+                    var vertex_index = x + y * terrain_dim;
+
+                    var normal_sum = glMatrix.vec3.create();
+
+                    // Iterate through all adjacent (connected) vertices, aka,
+                    // the vertices which share a triangle with this vertex.
+                    // For each of the connected vert
+                    adjacency_map[vertex_index].forEach((tri_idx) => {
+                        glMatrix.vec3.add(normal_sum, normal_sum, tri_norms[tri_idx/3]);
+                    });
+                    
+                    // Normalize vertex normal.
+                    glMatrix.vec3.normalize(normal_sum, normal_sum);
+
+                    vertexNormals.push(normal_sum[0], normal_sum[1], normal_sum[2]);
+                }
+            }
+        } else { // Else, if generateSphere == true
+
+            // Generate basic sphere. (Also generates element indices and normals)
             sphereFromSubdivision(4, modelData, modelIndices, vertexNormals);
             console.log(modelData.length/3 + " vertices");
             console.log(modelIndices.length/3 + " triangles");
@@ -278,25 +291,19 @@ terrain_demo = function() {
             }
         }
         
-        
+        // Construct the actual WebGL buffers.
 
-        // Setup the position buffer.
         var vertexPositionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(modelData), drawMode);
-        //vertexPositionBuffer.itemSize = 3;
-        //vertexPositionBuffer.numberOfItems = 3;
 
         var vertexIndexBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(modelIndices), drawMode);
-        //vertexIndexBuffer.itemSize = 3;
-        //vertexIndexBuffer.numberOfItems = modelIndices/3;
 
         var vertexNormalBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), drawMode);
-
 
         return {positions: vertexPositionBuffer, indices: vertexIndexBuffer, normals: vertexNormalBuffer, numIndices: modelIndices.length};
     };
@@ -305,8 +312,9 @@ terrain_demo = function() {
     
 
     /**
-     * Draws the logo.
-     * @param {number} time Current time variable used for animation
+     * Draws the terrain demo.
+     * @param {number} x Rotation factor
+     * @param {number} y Dolly zoom factor
      * @param {number} num_polys Number of triangles to draw
      * @param {WebGLBuffer} vertexPositionBuffer WebGLBuffer for vertex position data
      * @param {WebGLBuffer} vertexIndexBuffer WebGLBuffer for vertex index data
@@ -314,38 +322,40 @@ terrain_demo = function() {
      * @param {WebGLRenderingContext} gl Reference to the WebGL context
      */
     function draw(x, y, num_polys, vertexPositionBuffer, vertexIndexBuffer, vertexNormalBuffer, shaderProgram, gl){
+
+        // Compute the various matrices for this frame
         calculate_matrices(x - 250, y, shaderProgram, gl);
+
+        // Face culling is off by artistic choice, it looks kind of neat
+        // to be able to see the underside of the terrain!
         //gl.enable(gl.CULL_FACE);
+
+
+        // Set all the GL flags we need
         gl.enable(gl.DEPTH_TEST);
-        //gl.depthMask(gl.GL_TRUE);
-        //gl.depthFunc(gl.LEQUAL);
         gl.depthRange(-1.0, 1.0);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
         gl.clear(gl.COLOR_BUFFER_BIT);
         gl.clearDepth(1);
 
-
-        gl.uniform1f(shaderProgram.scaleFactor, 0.5);
-        gl.uniform1f(shaderProgram.scaleFactor2, 0.5);
+        // Configure all our buffers and attributes
 
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
         gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
-
         
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
         gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 
                                     3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute)
         
-                    
+        // Go draw 'em!
         gl.drawElements(gl.TRIANGLES, num_polys, gl.UNSIGNED_SHORT, 0);
     };
-    
 
-
+    // The sketchy VanillaJS equivalent of module.exports
     return {
         runDemo: runDemo
     };
