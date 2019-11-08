22b2c266f5ad2a776bcfef396ac21c46c155c798
Finn Sinclair <finnnorth@gmail.com>
2019-11-08 03:30:07 -0600

    Environment mapping works

--- mp3/scripts/envmap_demo.js
+++ mp3/scripts/envmap_demo.js
@@ -5,33 +5,6 @@
 /** @global Toggle for whether to use the sphere visualization option */
 var useSphere = false;
 
-/** @global Flag for changed sphere state */
-var regenModel = false;
-
-/** @global Constant for terrain tile size (terrain_dim x terrain_dim tiles) */
-const terrain_dim = 128;
-
-/** @global Current user/pilot position */
-var user_pos = glMatrix.vec3.create();
-/** @global Current user/pilot rotation quaternion */
-var user_rotation = glMatrix.quat.create();
-/** @global Current user/pilot velocity (scalar, units per frame) */
-var user_velocity = 0.005;
-/** @global Current user/pilot angular velocity */
-var user_angular_velocity = glMatrix.vec3.create();
-
-/** @global Skybox mesh data (vertices) */
-var skyboxVertexPosArray = [];
-/** @global Skybox mesh data (indices) */
-var skyboxIndexArray = [];
-/** @global Skybox mesh data (normals) */
-var skyboxNormalArray = [];
-/** @global Skybox WebGL buffer (vertices) */
-var skybox_vertexPositionBuffer;
-/** @global Skybox WebGL buffer (indices) */
-var skybox_vertexIndexBuffer;
-/** @global Skybox WebGL buffer (normals) */
-var skybox_vertexNormalBuffer
 
 
 
@@ -52,16 +25,66 @@ envmap_demo = function() {
         var canvas = document.getElementById("myGLCanvas");
         var gl = WebGLUtils.setupWebGL(canvas);
 
-        // Shader program
+        const faceInfos = [
+            {
+              target: gl.TEXTURE_CUBE_MAP_POSITIVE_X, 
+              url: 'envmap/pos-x.jpg',
+            },
+            {
+              target: gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 
+              url: 'envmap/neg-x.jpg',
+            },
+            {
+              target: gl.TEXTURE_CUBE_MAP_POSITIVE_Y, 
+              url: 'envmap/pos-y.jpg',
+            },
+            {
+              target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, 
+              url: 'envmap/neg-y.jpg',
+            },
+            {
+              target: gl.TEXTURE_CUBE_MAP_POSITIVE_Z, 
+              url: 'envmap/pos-z.jpg',
+            },
+            {
+              target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, 
+              url: 'envmap/neg-z.jpg',
+            },
+        ];
+
+        // Box shader program
         var boxShaderProgram; 
         var boxShaderUniforms = [
             "u_modelview",
             "u_projection",
             "u_normalMatrix",
             "u_shininess",
+            "u_baseColor",
+            "u_cubemap",
+            "u_worldCameraPosition",
+            "u_time"
         ]
-        boxShaderProgram = setupShaders(gl, "box-shader-vs", "box-shader-fs", boxShaderUniforms);
+        var boxShaderAttributes = [
+            "aVertexPosition",
+            "aVertexNormal"
+        ];
+        boxShaderProgram = setupShaders(gl, "box-shader-vs", "box-shader-fs", boxShaderUniforms, boxShaderAttributes);
         if(boxShaderProgram == null){
+            alert("shader program is null");
+        }
+
+        // Sky shader program
+        var skyShaderProgram; 
+        var skyShaderUniforms = [
+            "u_vpInverse",
+            "u_cubemap"
+        ]
+        var skyShaderAttributes = [
+            "aVertexPosition",
+            "aVertexNormal"
+        ];
+        skyShaderProgram = setupShaders(gl, "sky-shader-vs", "sky-shader-fs", skyShaderUniforms, skyShaderAttributes);
+        if(skyShaderProgram == null){
             alert("shader program is null");
         }
         
@@ -80,15 +103,31 @@ envmap_demo = function() {
         vertexNormalBuffer = bufferResult.normals;
         var rotateX = 0;
         var dollyY = 1200;
+        
+        setupCubemap(faceInfos, gl);
+        
+        gl.useProgram(boxShaderProgram);
+        gl.uniform1i(boxShaderProgram.uniforms["u_cubemap"], 0);
+        gl.useProgram(skyShaderProgram);
+        gl.uniform1i(skyShaderProgram.uniforms["u_cubemap"], 0);
+
         function render(now) {
 
 
             // Draw scene, pass in buffers
-            draw(rotateX, dollyY * 0.001, bufferResult.numIndices, vertexPositionBuffer, vertexIndexBuffer, vertexNormalBuffer, boxShaderProgram, gl);
+            //draw(rotateX, dollyY * 0.001, bufferResult, boxShaderProgram, gl);
             
+            draw_sky(rotateX, dollyY * 0.001, bufferResult, skyShaderProgram, gl);
+            draw_model(rotateX, dollyY * 0.001, bufferResult, boxShaderProgram, gl);
+
+            gl.uniform1f(boxShaderProgram.uniforms["u_time"], now);
+
             // Set the uniform value for the shininess based on slider input.
             gl.uniform1f(boxShaderProgram.uniforms["u_shininess"], document.getElementById("shininess").value);
 
+            gl.uniform4fv(boxShaderProgram.uniforms["u_baseColor"], [0.0, 1.0, 1.0, 1.0]);
+
+            rotateX += 0.01;
             animationID = requestAnimationFrame(render);
         }
         animationID = requestAnimationFrame(render);
@@ -102,7 +141,7 @@ envmap_demo = function() {
      * @param {number} y Y factor (used to dolly-zoom the camera)
      * @param {WebGLProgram} shaderProgram Reference to the shader program it will update uniform matrices for
      * @param {WebGLRenderingContext} gl Reference to the WebGL context
-     * @returns void
+     * @returns set of matrices
      */
     var calculate_matrices = function(x, y, shaderProgram, gl){
 
@@ -113,7 +152,10 @@ envmap_demo = function() {
         // Initialize view matrix.
         var viewMatrix = glMatrix.mat4.create();
 
-        glMatrix.mat4.lookAt(viewMatrix, [4,4,4], [0,0,0], [0,1,0]);
+        glMatrix.mat4.lookAt(viewMatrix, [2*Math.cos(x),2*Math.sin(x),2], [0,0,0], [0,1,0]);
+
+
+        gl.uniform3fv(shaderProgram.uniforms["u_worldCameraPosition"], [2*Math.cos(x),2*Math.sin(x),2]);
 
         // Init model matrix (this is just placeholder, not currently used)
         var modelMatrix = glMatrix.mat4.create();
@@ -129,11 +171,21 @@ envmap_demo = function() {
         glMatrix.mat3.fromMat4(normalMatrix, modelViewMatrix);
         glMatrix.mat3.transpose(normalMatrix, normalMatrix);
         glMatrix.mat3.invert(normalMatrix, normalMatrix);
-        
-        // Load the matrices into the shader program!
-        gl.uniformMatrix4fv(shaderProgram.uniforms["u_modelview"], false, modelViewMatrix);
-        gl.uniformMatrix4fv(shaderProgram.uniforms["u_projection"], false, projectionMatrix);
-        gl.uniformMatrix3fv(shaderProgram.uniforms["u_normalMatrix"], false, normalMatrix);
+
+        var newViewMatrix = glMatrix.mat4.create();
+        glMatrix.mat4.lookAt(newViewMatrix, [0,0,0], [-2*Math.cos(x),-2*Math.sin(x),-2], [0,1,0]);
+
+        var invViewProjMatrix = glMatrix.mat4.create();
+
+        glMatrix.mat4.multiply(invViewProjMatrix, projectionMatrix, newViewMatrix);
+        glMatrix.mat4.invert(invViewProjMatrix, invViewProjMatrix);
+
+        return {
+            modelview: modelViewMatrix,
+            invViewProj: invViewProjMatrix,
+            projection: projectionMatrix,
+            normal: normalMatrix
+        }
     };
     
     
@@ -143,10 +195,11 @@ envmap_demo = function() {
      * @param {WebGLRenderingContext} gl Reference to the WebGL context
      * @param {string} vertexShader Name of the vertex shader
      * @param {string} fragmentShader Name of the fragment shader
-     * @param {Array} uniforms Array of attributes to map
+     * @param {Array} uniforms Array of uniforms to map
+     * @param {Array} attributes Array of attributes to map
      * @returns {WebGLProgram} The newly created shader program.
      */
-    var setupShaders = function(gl, vertexShaderName, fragmentShaderName, uniforms) {
+    var setupShaders = function(gl, vertexShaderName, fragmentShaderName, uniforms, attributes) {
         vertexShader = loadShader(vertexShaderName, gl);
         fragmentShader = loadShader(fragmentShaderName, gl);
 
@@ -161,11 +214,18 @@ envmap_demo = function() {
     
         gl.useProgram(shaderProgram);
 
-        shaderProgram.uniforms = new Object();
+        shaderProgram.uniforms = {};
 
         for(var uniform of uniforms){
             shaderProgram.uniforms[uniform] = gl.getUniformLocation(shaderProgram, uniform);
         }
+
+        shaderProgram.attributes = {};
+
+        for(var attr of attributes){
+            shaderProgram.attributes[attr] = gl.getAttribLocation(shaderProgram, attr);
+        }
+
         return shaderProgram;
     };
 
@@ -188,12 +248,6 @@ envmap_demo = function() {
         sphereFromSubdivision(4, modelData, modelIndices, vertexNormals);
         console.log(modelData.length/3 + " vertices");
         console.log(modelIndices.length/3 + " triangles");
-
-        for(var i = 0; i < modelData.length; i+=3){
-            modelData[i] += terrain_dim*0.05;
-            modelData[i+1] += 1.2;
-            modelData[i+2] += terrain_dim*0.05;
-        }
         
         // Construct the actual WebGL buffers.
 
@@ -209,14 +263,46 @@ envmap_demo = function() {
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), drawMode);
         
-        
         return { positions: vertexPositionBuffer,
                  indices: vertexIndexBuffer,
                  normals: vertexNormalBuffer,
                  numIndices: modelIndices.length };
     };
 
+    /**
+     *  Sets up cubemap texture.
+     * @param {number} faces Collection of cubemap face data
+     * @param {WebGLRenderingContext} gl Reference to the WebGL context
+     */
+    var setupCubemap = function(faces, gl) {
+        var texture = gl.createTexture();
 
+        gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+
+        faces.forEach((info) => {
+
+            const level = 0;
+            const internalFormat = gl.RGBA;
+            const width = 512;
+            const height = 512;
+            const format = gl.RGBA;
+            const type = gl.UNSIGNED_BYTE;
+
+            gl.texImage2D(info.target, level, internalFormat, width, height, 0, format, type, null);
+
+            const image = new Image();
+            image.src = info.url;
+            image.onload = () => {
+                gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+                gl.texImage2D(info.target, level, internalFormat, format, type, image);
+                gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+            };
+
+        });
+
+        gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    }
     
 
     /**
@@ -229,62 +315,85 @@ envmap_demo = function() {
      * @param {WebGLProgram} shaderProgram Reference to the shader program it will update uniform matrices for
      * @param {WebGLRenderingContext} gl Reference to the WebGL context
      */
-    function draw(x, y, num_polys, vertexPositionBuffer, vertexIndexBuffer, vertexNormalBuffer, shaderProgram, gl){
+    function draw_model(x, y, buffers, shaderProgram, gl){
+
+        gl.useProgram(shaderProgram);
 
         // Compute the various matrices for this frame
-        calculate_matrices(x - 250, y, shaderProgram, gl);
+        var matResult = calculate_matrices(x, y, shaderProgram, gl);
+
+        gl.uniformMatrix4fv(shaderProgram.uniforms["u_modelview"], false, matResult.modelview);
+        gl.uniformMatrix4fv(shaderProgram.uniforms["u_projection"], false, matResult.projection);
+        gl.uniformMatrix3fv(shaderProgram.uniforms["u_normalMatrix"], false, matResult.normal);
 
         // Face culling is off by artistic choice, it looks kind of neat
         // to be able to see the underside of the terrain!
         //gl.enable(gl.CULL_FACE);
 
 
-        // Set all the GL flags we need
-        gl.enable(gl.DEPTH_TEST);
-        gl.depthRange(-1.0, 1.0);
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.clearDepth(1);
 
         // Configure all our buffers and attributes
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.positions);
+        gl.vertexAttribPointer(shaderProgram.attributes["aVertexPosition"], 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shaderProgram.attributes["aVertexPosition"]);
 
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
         
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.normals);
+        gl.vertexAttribPointer(shaderProgram.attributes["aVertexNormal"], 
                                     3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute)
-        
-        // Tell the shader we're currently NOT drawing the skybox
-        gl.uniform1f(shaderProgram.skyboxDraw, 0.0);
+        gl.enableVertexAttribArray(shaderProgram.attributes["aVertexNormal"])
         // Go draw 'em!
-        gl.drawElements(gl.TRIANGLES, num_polys, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, buffers.numIndices, gl.UNSIGNED_SHORT, 0);
+
+
+    };
+
+    /**
+     * Draws the terrain demo.
+     * @param {number} x Rotation factor
+     * @param {number} y Dolly zoom factor
+     * @param {number} num_polys Number of triangles to draw
+     * @param {WebGLBuffer} vertexPositionBuffer WebGLBuffer for vertex position data
+     * @param {WebGLBuffer} vertexIndexBuffer WebGLBuffer for vertex index data
+     * @param {WebGLProgram} shaderProgram Reference to the shader program it will update uniform matrices for
+     * @param {WebGLRenderingContext} gl Reference to the WebGL context
+     */
+    function draw_sky(x, y, buffers, shaderProgram, gl){
+
+        gl.useProgram(shaderProgram);
+
+        var matResult = calculate_matrices(x, y, shaderProgram, gl);
+
+        gl.uniformMatrix4fv(shaderProgram.uniforms["u_vpInverse"], false, matResult.invViewProj);
+
+        // Set all the GL flags we need
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clearDepth(1.);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthRange(0.0, 1.0);
+
+        // Construct skybox plane
+        var plane = new Float32Array(
+        [
+            -1, -1, 
+            1, -1, 
+            -1,  1, 
+            -1,  1,
+            1, -1,
+            1,  1,
+        ]);
+        gl.bufferData(gl.ARRAY_BUFFER, plane, gl.STATIC_DRAW);
+        gl.vertexAttribPointer(shaderProgram.attributes["aVertexPosition"],
+                                2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shaderProgram.attributes["aVertexPosition"]);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
 
         
 
-        // Draw skybox!
-        // We re-bind the buffers to the skybox buffers (which are global)
-        gl.bindBuffer(gl.ARRAY_BUFFER, skybox_vertexPositionBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, skybox_vertexIndexBuffer);
-        
-        gl.bindBuffer(gl.ARRAY_BUFFER, skybox_vertexNormalBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 
-                                    3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute)
-        
-        // Tell the shader program we're currently drawing the skybox
-        gl.uniform1f(shaderProgram.skyboxDraw, 1.0);
-        // Go draw 'em!
-        gl.drawElements(gl.TRIANGLES, skyboxIndexArray.length, gl.UNSIGNED_SHORT, 0);
-
-        
 
     };
 
